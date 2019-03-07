942dc2e86b2d06777330a04136790e896f132f00
jessehang <hangtimee@gmail.com>
2019-03-06 17:08:29 -0800

    Finished Tests

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Search from './../components/Search.js';
 import { connect } from 'react-redux';
-import handleSearchChange from '../actions/search.js';
+// import handleSearchChange from '../actions/search.js';
 import {applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import handleVideoSearch from '../actions/search.js';
 
 
-const SearchContainer = connect(<Search/>, applyMiddleware(thunk));
+const mapStateToProps = state => {
+    
+    return {
+        value: state.value
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleSearchInputChange: (video) => dispatch(handleVideoSearch(video))
+    }
+}
+const SearchContainer = connect(mapStateToProps, mapDispatchToProps)(Search)
 
 //TODO: define a SearchContainer component which will hook up your action
 // dispatchers with your search component props.
